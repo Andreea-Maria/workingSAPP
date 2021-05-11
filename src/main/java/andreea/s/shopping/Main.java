9bf0586a0a0d7d6715fbee2b6b978f200d87f193
adaie2002 <xsxa24@gmail.com>
2021-05-11 23:45:19 +0300

    scene builder

--- src/main/java/andreea/s/shopping/Main.java
+++ src/main/java/andreea/s/shopping/Main.java
@@ -1,24 +1,29 @@
 package andreea.s.shopping;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Shopping app");
-       Scene scene = new Scene(label);
-       primaryStage.setScene(scene);
-       primaryStage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        primaryStage.setTitle("Test Aplicatie shopp");
+        primaryStage.setScene(new Scene(root,300,300));
+        primaryStage.show();
 
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/main/java/andreea/s/shopping/Sample.java
@@ -0,0 +1,4 @@
+package andreea.s.shopping;
+
+public class Sample {
+}
